1c58762c015ca1b3395e7e7852feaf906060842b
davuush <133566082+davuush@users.noreply.github.com>
2025-07-07 13:35:46 +0300

    first commit

---
+++ js/lesson1.js
@@ -0,0 +1,40 @@
+// // let,cont,var
+// const firstName = "David";
+// console.log(firstName);
+// let lastName;
+
+// lastName = "Usharuk"
+// console.log(lastName);  
+
+
+// // 7 премітивних видів данних   string   number   bigint   boolean   symbol   null   underfined
+// const name = "beep";
+// console.log(typeof(name));
+
+// const age = 7;
+// console.log(typeof(age));
+
+// console.log(10>12);
+// console.log(typeof(10>12));
+// console.log(typeof(%));
+
+// const message = "Я ВИчаю ДЖавА скРІпт";
+// console.log(message.length);
+
+
+// let number = "21";
+// console.log(number);
+
+
+// const upperMessage = message.toUpperCase();
+// console.log(upperMessage);
+
+
+// const lowerMessage = message.toLowerCase();
+// console.log(lowerMessage);
+
+
+// alert(message);
+
+
+// const result = confirm("чи виповнилося вам 18 років?")
