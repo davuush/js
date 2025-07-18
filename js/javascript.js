cceeaf8a885cfa68660596a021043e251bfdc8b1
davuush <133566082+davuush@users.noreply.github.com>
2025-07-18 21:07:27 +0300

    hm2

--- js/javascript.js
+++ js/javascript.js
@@ -1,37 +1,37 @@
-const age = 13;
-console.log(age);
+// const age = 13;
+// console.log(age);
 
 
-const name = "Давид";
-console.log(name);
+// const name = "Давид";
+// console.log(name);
 
 
-const isStudent = true;
-console.log(isStudent);
+// const isStudent = true;
+// console.log(isStudent);
 
 
-const myString = "У будь-якому процесі важлива не швидкість, а задоволення";
-console.log(myString);
+// const myString = "У будь-якому процесі важлива не швидкість, а задоволення";
+// console.log(myString);
 
 
-let myNumber = 4;
-myNumber =  myNumber + 10;
-console.log(myNumber);
+// let myNumber = 4;
+// myNumber =  myNumber + 10;
+// console.log(myNumber);
 
 
-const myNull = null;
-console.log(myNull);
+// const myNull = null;
+// console.log(myNull);
+    
+
+// let userName = prompt("Введіть своє ім’я");
+// alert(userName);
+// console.log(userName);
 
 
-let userName = prompt("Введіть своє ім’я");
-alert(userName);
-console.log(userName);
+// let userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+// console.log(userAnswer);
 
 
-let userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
-console.log(userAnswer);
-
-
-alert("Увага! Дія може бути небезпечною.");
-let agree = confirm("Ви впевнені, що хочете продовжити?")
-console.log(agree);
+// alert("Увага! Дія може бути небезпечною.");
+// let agree = confirm("Ви впевнені, що хочете продовжити?")
+// console.log(agree);
