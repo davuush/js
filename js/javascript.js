72d9acafddb7234ebb4e2014b806bb20a1aff324
davuush <133566082+davuush@users.noreply.github.com>
2025-08-16 19:05:43 +0300

    hm6

--- js/javascript.js
+++ js/javascript.js
@@ -288,3 +288,275 @@
 // }
 
 
+// const drinks =  parseInt(prompt("Виберіть який напій будете: 1-кава 2-чай 3-сік").trim());
+
+// switch (drinks){
+//     case 1:
+//         alert("Ви обрали напій кава")
+//         break;
+//     case 2:
+//         alert("Ви обрали напій чай")
+//         break;
+//     case 3:
+//         alert("Ви обрали напій сік")
+//         break;
+//     default:
+//         alert("Такого варіанту відповіді нема")
+//         break;
+// }
+
+
+// const days = parseInt(prompt("Виберіть любий день тижня: 1-Понеділок  2-Вівторок  3-Середа  4-Четвер  5-Пятниця  6-Субота  7-Неділя").trim());
+
+// switch (days){
+//     case 1:
+//         alert("Ви обрали день Понеділок")
+//         break;
+//     case 2:
+//         alert("Ви обрали день Вівторок")
+//         break;
+//     case 3:
+//         alert("Ви обрали день Середа")
+//         break;
+//     case 4:
+//         alert("Ви обрали день Четвер")
+//         break;
+//     case 5:
+//         alert("Ви обрали день Пятниця")
+//         break;
+//     case 6:
+//         alert("Ви обрали день Субота")
+//         break;
+//     case 7:
+//         alert("Ви обрали напій Неділя")
+//         break;
+//     default:
+//         alert("Такого варіанту відповіді нема")
+//         break;
+// }
+
+
+// const month = parseInt(prompt("Введіть число під яким стоіть ваш забажаний місяць").trim());
+
+// switch (month){
+//     case 1:
+//     case 2:
+//     case 12:
+//         alert("Ваша пора року зима")
+//         break;
+//     case 3:
+//     case 4:
+//     case 5:
+//         alert("Ваша пора року весна")
+//         break;
+//     case 6:
+//     case 7:
+//     case 8:
+//         alert("Ваша пора року літо")
+//         break;
+//     case 9:
+//     case 10:
+//     case 11:
+//         alert("Ваша пора року осінь")
+//         break;
+//     default:
+//         alert("Ви не вибрали жодного місяця отже вам не вказано жодноі пори року");
+//         break;
+// }
+
+
+// const color = prompt("Напишыть любий колір світлофора і ми вкажем ваші подальші діЇ").toLowerCase().trim();
+
+
+// switch (color){
+//     case "червоний":
+//         alert("зупиніться")
+//         break;
+//     case "жовтий":
+//         alert("очікуйте")
+//         break;
+//     case "зелений":
+//         alert("проїзжайте")
+//         break;
+//     default:
+//         alert("ви не написала жодного коліру який є у світлофорі")
+//         break;
+// }
+
+
+
+// const firstNumber = Number(prompt("Введіть перше число:").trim());
+// const secondNumber = Number(prompt("Введіть друге число:").trim());
+
+// const operator = prompt("Введіть оператор (+, -, *, /)").trim();
+
+// let result;
+
+// switch(operator){
+//     case "+":
+//         result = firstNumber + secondNumber
+//         break;
+//      case "-":
+//         result = firstNumber - secondNumber;
+//         break;
+//     case "*":
+//         result = firstNumber * secondNumber;
+//         break;
+//     case "/":
+//         if (secondNumber === 0){
+//             alert("ділення на ноль неможливе і неможна")
+//         } else {
+//             result = firstNumber / secondNumber
+//         }
+//         break;
+//     default:
+//         alert("ви не вказали потрібного оператора")
+//         break;
+
+// }
+//     alert("Ваш результат:" + result)
+// for (let i = 0; i <=10; i += 1;){
+//     console.log(i);
+// }
+
+// const userInfo = "Мене звати давид я начаюся в GoIteens";
+
+
+
+// for(let i = 0; i < userInfo.length; i += 1){
+//     if(userInfo[i] === " "){
+//         continue
+//     } 
+
+//         console.log(userInfo[i]);
+// }
+
+// for(let i = 0; i < userInfo.length; i += 1){
+//     if(userInfo[i] === " "){
+//         continue
+//     }
+//     if(userInfo[i] === "G"){
+//         break
+//     } 
+
+//         console.log(userInfo[i]);
+// }
+
+
+
+// const numbers = ["+133 1131 131 1111 ","+334 4000 113 9090","+111 1231 656 7777","+909 1234 678 9999","+898 9833 123 6767","+791 1371 313 111"]
+// const findToNumbers = "+909 1234 678 9999"
+// for(let i = 0; i < numbers.length; i += 1){
+//     if(numbers[i] = findToNumbers){
+//         console.log(`Ура ми найшли потрібний нам номер : ${numbers[i]}`);
+//         break
+//     }
+// }
+
+
+
+
+
+
+// let counter = 0;
+// while (counter < 50){
+//     console.log(counter);
+//     counter += 1;
+    
+// }
+
+
+// for(let counter = 0; counter < 50; counter += 1){
+//     console.log(counter);
+    
+// 
+
+
+// let counter = 50;
+// while (counter >= 0){
+//     console.log(counter);
+//     counter -= 1;
+    
+// }
+
+
+// for(let counter = 50 ; counter > 0; counter -= 1){
+//     console.log(counter);
+    
+// }
+
+
+
+//Задача 3: Порахувати суму чисел від 1 до 10 (while)
+// let counter = 0;
+// let suma = 0;
+// while (counter <= 10){
+//     suma +=  counter
+
+//     counter += 1;
+// }
+//     console.log(suma);
+
+
+
+// let sum = 0;
+// for(let i = 0 ; i <= 10 ; i += 1){
+
+//     sum += 1;
+// }
+//     console.log(sum); 
+
+
+
+
+// for (let number = 1; number < 10 ; number += 2){
+//     console.log(number);
+    
+// }
+
+// let number = 1 ;
+// while( number < 10){
+//         console.log(number);
+//     number += 2;
+
+// }
+
+// let counter = 0;
+// while (counter <= 20){
+//     counter += 1;
+//     if(counter === 10){
+//         continue
+//     }
+// }
+
+
+// let num;
+// do {
+//     num = parseInt(prompt("введіть число більше 10"))
+// }while (num <= 10)
+
+
+
+// let num;
+
+// do {
+//     num = Number(prompt("Ведіть число більше 0"));
+    
+// }while (num <= 0 || isNaN(num)) 
+//     console.log(num);
+
+// let num;
+// do {
+//     num = Number(prompt("введіть номер телефону 10 цифр"))
+//     if( num === null){
+//         alert("дія скасована")
+//         break
+//     }
+//     if(num.length === 10 && isNaN(num)){
+//         alert=("номер телефону прийнято")
+//         break;
+//     }else{
+//         alert("невірні данні")
+//     }
+// } while (true)
+
--- js/lesson1.js
+++ js/lesson1.js
@@ -1,127 +1,47 @@
-const drinks =  parseInt(prompt("Виберіть який напій будете: 1-кава 2-чай 3-сік").trim());
-
-switch (drinks){
-    case 1:
-        alert("Ви обрали напій кава")
-        break;
-    case 2:
-        alert("Ви обрали напій чай")
-        break;
-    case 3:
-        alert("Ви обрали напій сік")
-        break;
-    default:
-        alert("Такого варіанту відповіді нема")
-        break;
+let i = 0;
+while (i <= 10){
+    console.log(i);
+    i = i + 1;
+    
 }
 
-
-const days = parseInt(prompt("Виберіть любий день тижня: 1-Понеділок  2-Вівторок  3-Середа  4-Четвер  5-Пятниця  6-Субота  7-Неділя").trim());
-
-switch (days){
-    case 1:
-        alert("Ви обрали день Понеділок")
-        break;
-    case 2:
-        alert("Ви обрали день Вівторок")
-        break;
-    case 3:
-        alert("Ви обрали день Середа")
-        break;
-    case 4:
-        alert("Ви обрали день Четвер")
-        break;
-    case 5:
-        alert("Ви обрали день Пятниця")
-        break;
-    case 6:
-        alert("Ви обрали день Субота")
-        break;
-    case 7:
-        alert("Ви обрали напій Неділя")
-        break;
-    default:
-        alert("Такого варіанту відповіді нема")
-        break;
-}
-
-
-const month = parseInt(prompt("Введіть число під яким стоіть ваш забажаний місяць").trim());
-
-switch (month){
-    case 1:
-    case 2:
-    case 12:
-        alert("Ваша пора року зима")
-        break;
-    case 3:
-    case 4:
-    case 5:
-        alert("Ваша пора року весна")
-        break;
-    case 6:
-    case 7:
-    case 8:
-        alert("Ваша пора року літо")
-        break;
-    case 9:
-    case 10:
-    case 11:
-        alert("Ваша пора року осінь")
-        break;
-    default:
-        alert("Ви не вибрали жодного місяця отже вам не вказано жодноі пори року");
-        break;
-}
-
-
-const color = prompt("Напишыть любий колір світлофора і ми вкажем ваші подальші діЇ").toLowerCase().trim();
-
-
-switch (color){
-    case "червоний":
-        alert("зупиніться")
-        break;
-    case "жовтий":
-        alert("очікуйте")
-        break;
-    case "зелений":
-        alert("проїзжайте")
-        break;
-    default:
-        alert("ви не написала жодного коліру який є у світлофорі")
-        break;
+for (let q = 2 ; q <= 20 ; q += 2){
+      if (q % 2 !== 0) {
+        continue; 
+  }
+  console.log(q);
+  
 }
 
 
 
-const firstNumber = Number(prompt("Введіть перше число:").trim());
-const secondNumber = Number(prompt("Введіть друге число:").trim());
 
-const operator = prompt("Введіть оператор (+, -, *, /)").trim();
-
-let result;
-
-switch(operator){
-    case "+":
-        result = firstNumber + secondNumber
-        break;
-     case "-":
-        result = firstNumber - secondNumber;
-        break;
-    case "*":
-        result = firstNumber * secondNumber;
-        break;
-    case "/":
-        if (secondNumber === 0){
-            alert("ділення на ноль неможливе і неможна")
-        } else {
-            result = firstNumber / secondNumber
-        }
-        break;
-    default:
-        alert("ви не вказали потрібного оператора")
-        break;
-
+const number = 7;
+for(let s = 0; i <= 10; i+= 1){
+    console.log(`${number * s}`);
+    
 }
-    alert("Ваш результат:" + result)
+
+
+
+
+let fixedNumber = Number(prompt("введіть ваше число n"))
+for(let l = 0 ; l < fixedNumber ; l += 1){
+    if (l > fixedNumber){
+        break
+    }
+    console.log(l);
+    
+}
+
+
+
+let d = 1
+while(d <= 20){
+    d = d + 1
+          if (d % 3 === 0) {
+        continue; 
+  }
+  console.log(d);
+  
+}
